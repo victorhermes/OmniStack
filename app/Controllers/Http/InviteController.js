5ee4fedfb5c3998d85dd9c2526aa93885af1db95
Victor Hermes <me.victorhermes@gmail.com>
2019-03-21 22:36:43 -0300

    Clean

--- app/Controllers/Http/InviteController.js
+++ app/Controllers/Http/InviteController.js
@@ -1,12 +1,5 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
-/**
- * Resourceful controller for interacting with invites
- */
 class InviteController {
   async store ({ request, response }) {
     console.log(request.team)
