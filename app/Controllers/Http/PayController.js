3fce29253a2632f8e0a65519795adaaa4227cae9
Victor Hermes <me.victorhermes@gmail.com>
2019-05-07 23:08:03 -0300

    Payment

--- app/Controllers/Http/PayController.js
+++ app/Controllers/Http/PayController.js
@@ -3,26 +3,39 @@
 const pagarme = use('pagarme')
 
 class PayController {
-  async store ({ request }) {
-    const Cardnumber = request.body.Cardnumber
-
-    console.log(Cardnumber)
+  async store ({ request, response }) {
+    const {
+      CardNumber,
+      cardHolderName,
+      cardExpirationDate,
+      cardCvv,
+      installments
+    } = request.body
 
     await pagarme.client
       .connect({ api_key: 'ak_test_Qr7nr45sEh3K3P2MiFhsmgvuiWDWyV' })
       .then(client =>
         client.transactions.create({
           amount: 99000,
-          card_number: Cardnumber,
-          card_cvv: '123',
-          card_expiration_date: '0922',
-          card_holder_name: 'Vladimir Hermes',
-          installments: 2
+          card_number: CardNumber,
+          card_cvv: cardCvv,
+          card_expiration_date: cardExpirationDate,
+          card_holder_name: cardHolderName,
+          installments: installments
         })
       )
-      .then(transaction => console.log(transaction))
-
-    // return request.redirect('/')
+      .then(transaction => {
+        if (transaction.status === 'refused') {
+          return response
+            .status(404)
+            .send({ error: { message: 'Pagamento recusado' } })
+        }
+      })
+      .catch(() => {
+        return response
+          .status(404)
+          .send({ error: { message: 'Erro ao processar pagamento' } })
+      })
   }
 }
 
